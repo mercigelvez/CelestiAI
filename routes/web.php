ddfb5839e8fc52b84fccae760b76770700d24b14
Merciditas Gelvez <merciditasmayvgelvez@iskolarngbayan.pup.edu.ph>
2025-04-17 15:40:33 +0800

    🎱 Update web.php routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,50 @@
 <?php
+// routes/web.php
 
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\ForgotPasswordController;
+use App\Http\Controllers\Auth\ResetPasswordController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\ReadingController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Public routes
+Route::get('/', [WelcomeController::class, 'index'])->name('welcome');
+
+// Authentication routes
+Route::middleware('guest')->group(function () {
+    Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/login', [LoginController::class, 'login']);
+    Route::get('/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/register', [RegisterController::class, 'register']);
+    Route::get('/forgot-password', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
+    Route::post('/forgot-password', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('/reset-password/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
+    Route::post('/reset-password', [ResetPasswordController::class, 'reset'])->name('password.update');
+});
+
+// Authenticated routes
+Route::middleware('auth')->group(function () {
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Tarot reading routes
+    Route::get('/readings', [ReadingController::class, 'index'])->name('readings.index');
+    Route::get('/readings/new', [ReadingController::class, 'create'])->name('readings.create');
+    Route::post('/readings', [ReadingController::class, 'store'])->name('readings.store');
+    Route::get('/readings/{reading}', [ReadingController::class, 'show'])->name('readings.show');
+    Route::get('/readings/history', [ReadingController::class, 'history'])->name('readings.history');
 });
